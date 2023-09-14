c4178b2341e4ba25780009ad9795b633d0606737
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-15 00:20:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,63 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const paymentRoutes = require("./Routes/payment");
-const app = express();
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
 dotenv.config();
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/payment/", paymentRoutes);
-
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const instance = new Razorpay({
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET,
+});
+
+app.post("/api/payment/orders", async (req, res) => {
+  try {
+    const options = {
+      amount: req.body.amount * 100, // Amount in paise
+      currency: "INR",
+      receipt: crypto.randomBytes(10).toString("hex"),
+    };
+
+    instance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something Went Wrong!" });
+      }
+      res.status(200).json({ data: order });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error!" });
+    console.log(error);
+  }
+});
+
+app.post("/api/payment/verify", async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSign = crypto
+      .createHmac("sha256", process.env.KEY_SECRET)
+      .update(sign.toString())
+      .digest("hex");
+
+    if (razorpay_signature === expectedSign) {
+      return res.status(200).json({ message: "Payment verified successfully" });
+    } else {
+      return res.status(400).json({ message: "Invalid signature sent!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error!" });
+    console.log(error);
+  }
+});
