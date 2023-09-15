7c5727c2823453071251e8f403f57e4d3c049f08
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-15 13:44:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,31 @@ function App() {
   const [products, setProducts] = useState([
     {
       name: "Apple iPhone 13 (128GB) - Blue",
-      description: "The biggest Pro camera system upgrade ever. Super Retina XDR display with Pro - Motion for a faster, more responsive feel. Lightning-fast A15 Bionic chip. Durable design and a huge leap in battery life.",
+      description: "Features a powerful A15 Bionic chip, dual-camera system, Super Retina XDR display, and 128GB storage.",
       img: "https://m.media-amazon.com/images/I/71xb2xkN5qL._SX679_.jpg",
       price: 55999,
     },
     {
       name: "Apple iPhone 12 (256GB) - Purple",
-      description: "5G speed. A14 Bionic, the fastest chip in a smartphone. An edge-to-edge OLED display. Ceramic Shield with four times better drop performance. And Night mode on every camera.",
+      description: "Offers a stunning Super Retina XDR display, A14 Bionic chip, dual-camera setup, and ample 256GB storage.", 
       img: "https://m.media-amazon.com/images/I/71hIfcIPyxS._SX679_.jpg",
       price: 65999,
     },
     {
       name: "Apple iPhone SE (Black, 64 GB) (Includes EarPods, Power Adapter)",
-      description: "The iPhone SE (2nd generation) features a 4.7-inch Retina HD display, powered by the A13 Bionic chip for fast performance. It has a 12-megapixel camera, supports Touch ID for biometric security, and comes in storage options of 64GB, 128GB, and 256GB, offering a compact design similar to the iPhone 8 with the power of newer internals.",
+      description: "Compact design with an A13 Bionic chip, 64GB storage, and includes EarPods and a power adapter.", 
       img: "https://rukminim2.flixcart.com/image/416/416/k9loccw0/mobile/p/z/q/apple-iphone-se-mxd02hn-a-original-imafrcpjyscxm8fv.jpeg?q=70",
       price: 28990,
     },
     {
       name: "APPLE iPhone 11 Pro Max (Gold, 64 GB)",
-      description: "The Apple iPhone 11 Pro Max in Gold with 64 GB of storage boasts a stunning Super Retina XDR display, a powerful A13 Bionic chip for exceptional performance, and a versatile triple-lens camera system for capturing high-quality photos and videos. Its elegant design, Face ID for security, and long-lasting battery life make it a top-tier smartphone choice.",
+      description: "Premium design, A13 Bionic chip, triple-camera system, and 64GB storage capacity.", 
       img: "https://rukminim2.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/e/b/u/iphone-11-pro-max-64-a-mwhg2hn-a-apple-0-original-imafkg2fg3evmhuy.jpeg?q=70",
       price: 95699,
     },
     {
       name: "Apple iPhone XR (Black, 128 GB) (Includes EarPods, Power Adapter)",
-      description: "The Apple iPhone XR in Black with 128GB of storage includes EarPods and a power adapter. It offers a sleek design, a vibrant Liquid Retina display, and reliable performance for an enhanced mobile experience.",
+      description: "Featuring a Liquid Retina display, A12 Bionic chip, and 128GB storage, it includes EarPods and a power adapter.",
       img: "https://rukminim2.flixcart.com/image/416/416/jnj7iq80/mobile/u/b/g/apple-iphone-xr-mryj2hn-a-original-imafa6zkm7qhv2zd.jpeg?q=70",
       price: 41699,
     },
