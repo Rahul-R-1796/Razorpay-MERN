21a9b7329fe6f52e6c56bc382042ace86257c4f3
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-15 10:59:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,8 @@
-import React, { useState } from "react";
 import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [books, setBooks] = useState([
-    {
-      name: "World's Greatest Pack for Personal Growth and Wealth (Set of 4 Books)",
-      author: "by Dale Carnegie, Napoleon Hill, et al. | 1 August 2019",
-      img: "https://m.media-amazon.com/images/I/71frknp-CWL._AC_UY218_.jpg",
-      price: 325,
-    },
-    {
-      name: "To Kill a Mockingbird",
-      author: "by Harper Lee | 11 July 1960",
-      img: "https://m.media-amazon.com/images/I/51PLj80Oz-L._AC_UY218_.jpg",
-      price: 280,
-    },
-    {
-      name: "The Great Gatsby",
-      author: "by F. Scott Fitzgerald | 10 April 1925",
-      img: "https://m.media-amazon.com/images/I/81Yb-JC5VdL._AC_UY218_.jpg",
-      price: 220,
-    },
-    {
-      name: "Pride and Prejudice",
-      author: "by Jane Austen | 28 January 1813",
-      img: "https://m.media-amazon.com/images/I/81hSNh6jxjL._AC_UY218_.jpg",
-      price: 200,
-    },
-  ]);
-
   const [book, setBook] = useState({
     name: "The Fault In Our Stars",
     author: "John Green",
@@ -71,13 +44,13 @@ function App() {
     }
   };
 
-  const handlePayment = async (selectedBook) => {
+  const handlePayment = async () => {
     try {
       const response = await axios.post("/api/payment/orders", {
-        amount: selectedBook.price,
+        amount: book.price,
       });
       const orderId = response.data.data.id;
-      initPayment({ amount: selectedBook.price, currency: "INR", id: orderId });
+      initPayment({ amount: book.price, currency: "INR", id: orderId });
     } catch (error) {
       console.log(error);
     }
@@ -85,19 +58,17 @@ function App() {
 
   return (
     <div className="App">
-      {books.map((book, index) => (
-        <div className="book_container" key={index}>
-          <img src={book.img} alt="book_img" className="book_img" />
-          <p className="book_name">{book.name}</p>
-          <p className="book_author">By {book.author}</p>
-          <p className="book_price">
-            Price : <span>&#x20B9; {book.price}</span>
-          </p>
-          <button onClick={() => handlePayment(book)} className="buy_btn">
-            Buy Now
-          </button>
-        </div>
-      ))}
+      <div className="book_container">
+        <img src={book.img} alt="book_img" className="book_img" />
+        <p className="book_name">{book.name}</p>
+        <p className="book_author">By {book.author}</p>
+        <p className="book_price">
+          Price : <span>&#x20B9; {book.price}</span>
+        </p>
+        <button onClick={handlePayment} className="buy_btn">
+          Buy Now
+        </button>
+      </div>
     </div>
   );
 }
